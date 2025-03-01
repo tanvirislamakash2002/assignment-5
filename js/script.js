ae145f94e2b8268f67362b5de4965e298a6767ae
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-03-01 19:02:59 +0600

    clear history section done

--- js/script.js
+++ js/script.js
@@ -17,18 +17,38 @@ for(const btnElement of boxButton){
         numberReduce++
         document.getElementById('number-reduce').innerText = numberReduce
 
+
+        // activity massage  
+        const activityMassage = document.getElementById('activity-massage')
+        
+        const p = document.createElement('p');
+        
+
         const date = new Date()
         const hour = date.getHours()
         const minute = date.getMinutes()
         const second = date.getSeconds()
         
         const time = hour+":"+minute+":"+second+" AM";
-        document.getElementById('time').innerText ='You have Completed the task Fix Mobile Button Issue at '+ time;
+        p.innerHTML =`<span class = 'mb-2'>You have Completed the task Fix Mobile Button Issue at  ${time}</span>`;
+
+        activityMassage.appendChild(p);
+        p.setAttribute('class','mb-2')
+        p.classList.add('bg-gray-200')
+        p.classList.add('p-2')
+        p.classList.add('rounded-xl')
+
+
+        
 
 
 
         
     })
 }
+
+document.getElementById('clear-btn').addEventListener('click',function(){
+    document.getElementById('activity-massage').innerHTML=``;
+})
 
 
